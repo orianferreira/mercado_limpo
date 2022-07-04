889978e30e84c904852eaaaec8bd5b2a6b996086
Rian <brianalmeida998@gmail.com>
2022-07-04 09:01:39 -0300

    Criação arquivo Maven

---
+++ mercadolimpo/src/main/java/com/generation/mercadolimpo/MercadolimpoApplication.java
@@ -0,0 +1,13 @@
+package com.generation.mercadolimpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadolimpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadolimpoApplication.class, args);
+	}
+
+}
---
+++ mercadolimpo/src/test/java/com/generation/mercadolimpo/MercadolimpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.mercadolimpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadolimpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
