448b32bc64ee76a0bf32ecc14fc88b6c306540fc
WillianFerreira96 <willian.ferreira.gomes96@gmail.com>
2022-07-08 11:51:12 -0300

    atualização relacionamento

--- mercadolimpo/src/main/java/com/generation/mercadolimpo/model/Categoria.java
+++ mercadolimpo/src/main/java/com/generation/mercadolimpo/model/Categoria.java
@@ -1,12 +1,18 @@
 package com.generation.mercadolimpo.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "categoria")
@@ -23,6 +29,10 @@ public class Categoria {
 	@NotBlank
 	@Size(min = 10, max = 500)
 	private String tipo;
+
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public long getId() {
 		return id;
@@ -46,6 +56,13 @@ public class Categoria {
 
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
+	}
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 
 }
