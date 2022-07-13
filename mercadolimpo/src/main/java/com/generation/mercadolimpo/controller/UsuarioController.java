7a836c09cf014363d79d6fcbf6d502734068dbc1
Wanessa Koserski <wkoserski@gmail.com>
2022-07-13 14:59:29 -0300

    fix get usuario

--- mercadolimpo/src/main/java/com/generation/mercadolimpo/controller/UsuarioController.java
+++ mercadolimpo/src/main/java/com/generation/mercadolimpo/controller/UsuarioController.java
@@ -1,11 +1,13 @@
 package com.generation.mercadolimpo.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.mercadolimpo.model.Usuario;
 import com.generation.mercadolimpo.model.UsuarioLogin;
+import com.generation.mercadolimpo.repository.UsuarioRepository;
 import com.generation.mercadolimpo.service.UserService;
 
 @RestController
@@ -22,6 +25,14 @@ public class UsuarioController {
 
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private UsuarioRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> getAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
 	
 	@PostMapping("/login")
 	public ResponseEntity<UsuarioLogin> Authentication(@RequestBody Optional<UsuarioLogin> user) {
