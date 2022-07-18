9cbf1dd072eab59d977b93c13ea92841a35bc883
Wanessa Koserski <wkoserski@gmail.com>
2022-07-18 08:55:51 -0300

    atuallizar usuario controller

--- mercadolimpo/src/main/java/com/generation/mercadolimpo/controller/UsuarioController.java
+++ mercadolimpo/src/main/java/com/generation/mercadolimpo/controller/UsuarioController.java
@@ -3,12 +3,15 @@ package com.generation.mercadolimpo.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,23 +30,30 @@ public class UsuarioController {
 	private UserService userService;
 	
 	@Autowired
-	private UsuarioRepository repository;
+	private UsuarioRepository userRepository;
 	
-	@GetMapping
-	public ResponseEntity<List<Usuario>> getAll() {
-		return ResponseEntity.ok(repository.findAll());
+	@GetMapping("/all")
+	public ResponseEntity <List<Usuario>> getAll(){
+		return ResponseEntity.ok(userRepository.findAll());
 	}
 	
 	@PostMapping("/login")
 	public ResponseEntity<UsuarioLogin> Authentication(@RequestBody Optional<UsuarioLogin> user) {
-		return userService.Login(user).map(m -> ResponseEntity.ok(m))
+		return userService.login(user).map(m -> ResponseEntity.ok(m))
 															 .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
 	
 	@PostMapping("/cadastrar")
 	public ResponseEntity<Usuario> post(@RequestBody Usuario usuario) {
 		return ResponseEntity.status(HttpStatus.CREATED)
-												.body(userService.RegisterUser(usuario));
+												.body(userService.register(usuario));
+	}
+	
+	@PutMapping("/atualizar")
+	public ResponseEntity<Usuario> put(@Valid @RequestBody Usuario usuario) {
+		return userService.update(usuario)
+			.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+			.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
 	
 }
--- mercadolimpo/src/main/java/com/generation/mercadolimpo/service/UserService.java
+++ mercadolimpo/src/main/java/com/generation/mercadolimpo/service/UserService.java
@@ -7,6 +7,8 @@ import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
 
 import com.generation.mercadolimpo.model.Usuario;
 import com.generation.mercadolimpo.model.UsuarioLogin;
@@ -18,7 +20,8 @@ public class UserService {
 	@Autowired
 	private UsuarioRepository repository;
 	
-	public Usuario RegisterUser(Usuario usuario) {
+	
+	public Usuario register(Usuario usuario) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		
 		String passwordEncoder = encoder.encode(usuario.getSenha());
@@ -27,7 +30,8 @@ public class UserService {
 		return repository.save(usuario);
 	}
 	
-	public Optional<UsuarioLogin> Login(Optional<UsuarioLogin> user) {
+	
+	public Optional<UsuarioLogin> login(Optional<UsuarioLogin> user) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		Optional<Usuario> usuario = repository.findByEmail(user.get().getEmail());
 		
@@ -47,4 +51,28 @@ public class UserService {
 		return null;
 	}
 	
+	
+	public Optional<Usuario> update(Usuario usuario) {
+		
+		if(repository.findById(usuario.getId()).isPresent()) {
+			Optional<Usuario> buscaUsuario = repository.findByEmail(usuario.getEmail());
+				
+			if ( (buscaUsuario.isPresent()) && ( buscaUsuario.get().getId() != usuario.getId())) {
+				throw new ResponseStatusException(
+						HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
+			}		
+			usuario.setSenha(encryptPassword(usuario.getSenha()));
+			
+			return Optional.ofNullable(repository.save(usuario));	
+		}
+		
+		return Optional.empty();
+	}
+	
+	
+	private String encryptPassword(String senha) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();	
+		return encoder.encode(senha);
+
+	}
 }
